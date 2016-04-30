7175057605fc83a248439c07e1c2f32865f9896b
Chris Perrone <chrisp510@gmail.com>
2016-04-30 18:39:58 +0000

    Initial commit

---
+++ scroll.js
@@ -0,0 +1,58 @@
+/**
+ * Scroll.js
+ *
+ * A utility to permit infinite scrolling. Conserves memory
+ * usage by removing invisible elements from the DOM.
+ */
+
+var Scroll = function() {
+    this.disappearanceBuffer = 100; // Number of pixels above top of page before removing a card
+    this.initialCardCount = 5, // How many cards to init the environment with
+
+    this.container = $('.container'),
+    this.filler = $('.filler'),
+
+    // Counter to track number of cards we've seen
+    this.cardsThroughDom = 0,
+
+    // Initialization function
+    this.init = function() {
+        var self = this;
+
+        // Page scroll event
+        $(window).scroll(function() {
+            $('.card').each(function() {
+                var bottom = (this.offsetTop + this.offsetHeight);
+                if (bottom < window.pageYOffset - self.disappearanceBuffer) {
+                    // Add a new element at the bottom of the page
+                    self.container.append(self.newCard());
+                    // Stretch out filter above to compensate
+                    self.stretchFiller(this.offsetHeight + 30);
+                    // Delete offscreen element
+                    $(this).remove();
+                }
+            });
+        });
+
+        // Load the DOM with cards
+        for (var i = 0; i < this.initialCardCount; i++) {
+            this.container.append(self.newCard());
+        }
+
+        return this;
+    },
+
+    // Creates a new card
+    this.newCard = function() {
+        this.cardsThroughDom++;
+        return "<div class=\"card\" data-count=\"" + this.cardsThroughDom + "\"></div>";
+    },
+
+    // Stretches the page filler
+    this.stretchFiller = function(height) {
+        var current_height = this.filler.height();
+        this.filler.height(current_height + height);
+    };
+
+    return this.init();
+};
